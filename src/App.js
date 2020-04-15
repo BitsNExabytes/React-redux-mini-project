af1dcce86e5e280e11ad0b501d0e68b8f19105e9
chad Farrington <sandrock11@hotmail.com>
2020-04-14 20:38:37 -0400

    added all set up files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useEffect } from 'react';
+
+// bring in the main materialize
+import 'materialize-css/dist/css/materialize.min.css';
+
+// Bring in the javascript for modals
+import M from 'materialize-css/dist/js/materialize.min.js';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    //Initializes Materialize javascript
+    M.AutoInit();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment className='App'>
+      <h3>This is our app running</h3>
+    </Fragment>
   );
-}
+};
 
 export default App;
